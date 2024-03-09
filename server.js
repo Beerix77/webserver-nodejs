33aea06673ff1bc5c278cf9d84f47819ae559668
Andrew Matysiak <andrewmatysiak@hotmail.com>
2024-03-09 17:27:49 +1100

    Added a few more dynamic routes for testing

--- server.js
+++ server.js
@@ -15,19 +15,31 @@ app.listen( PORT, () => {
 // web server => map a path to some code:
 // app.verb( path, callback )
 
+
 // default path
 app.get('/', ( req, res) => { // res,req provided by 'express'
   
   console.log('Homepage selected....'); // terminal
   res.send('Hello Browser!!!'); // send to browser
 
-});
+}); // GET /
+
 
 
 // dynamic routes
-app.get ('/search/:subject', (req, res) => {
+app.get('/search/:subject', (req, res) => {
 
   console.log(`You are searching for ${req.params.subject}`);
   res.send(`RESULTS for: ${req.params.subject}`)
 
-});
+}); // GET /search/:subject
+
+
+
+app.get('/hello/:name/mood/:mood', (req, res) => {
+
+  res.send(`<h2>Hello ${req.params.name} you are ${req.params.mood.toUpperCase()}<h2>`);
+
+}); // GET /hello/:name/mood/:mood
+
+
